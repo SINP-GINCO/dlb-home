3d52cf246a3804afbed83b3e919508fcca827bfe
scandelier <severine.candelier@ign.fr>
2018-03-12 09:53:19 +0100

    Initial commit

---
+++ build_dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#------------------------------------------------------------------------------
+# Build the project for dev environment
+# parameters are all forward to composer.
+#------------------------------------------------------------------------------
+
+# Set permissions on cache and logs
+HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
+
+sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var/cache var/logs
+sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var/cache var/logs
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+        return $this->render('index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route("/legal", name="legal")
+     */
+    public function legal()
+    {
+        return $this->render('legal.html.twig', []);
+    }
+}
